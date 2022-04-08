b3f88dd693e15463e20135e7becba3ff78726a31
Rich Seeds <rich@techelevator.com>
2022-04-08 06:49:32 -0400

    Initial Commit

---
+++ java/src/test/resources/test-data.sql
@@ -0,0 +1,26 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS seq_user_id;
+
+CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+
+CREATE TABLE users (
+	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
+	username varchar(50) NOT NULL UNIQUE,
+	password_hash varchar(200) NOT NULL,
+	role varchar(50) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+INSERT INTO users (username,password_hash,role) VALUES ('user1','user1','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
+
+
+COMMIT TRANSACTION;
