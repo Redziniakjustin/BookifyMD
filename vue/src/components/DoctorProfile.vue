3bbfcf78fce5248c974224b85557f1416c1f6152
Martin Kyle <Kylesmartin98@gmail.com>
2022-04-14 22:29:07 -0400

    Updated DoctorProfile component

--- vue/src/components/DoctorProfile.vue
+++ vue/src/components/DoctorProfile.vue
@@ -9,11 +9,47 @@
               <td>{{doctor.phoneNumber}}</td><!--Phone # -->
           </tr>
       </tbody>
-    <div>
-      <button id="AddLocation"><button>
+      
+    <div id="buttonContainer">
+      <button id="AddLocation" v-onclick="addOffice = !addOffice">Add Hospital or Clinic</button>
       <button id="updateAvailability">Update Availability and Location</button>
       <button id="createAvailability">Create Availability</button>
       <button id="deleteAvailability">Delete Availability</button>
+    </div>
+    <div v-if="addOffice">
+      <form action="" @submit="Addlater">
+
+        <label for="officeName"></label>
+        <input type="text" id="officeName" v-model="office.officeName">
+       
+        <label for="officeAddress"></label>
+        <input type="text" id="officeAddress" v-model="office.officeAddress">
+       
+        <label for="officeCity"></label>
+        <input type="text" id="officeCity" v-model="office.officeCity">
+       
+        <label for="officeState"></label>
+        <input type="text" id="officeState" v-model="office.officeState">
+       
+        <label for="officeZipCode"></label>
+        <input type="text" id="officeZipCode" v-model="office.officeZipCode">
+       
+        <label for="officePhone">Phone: </label>
+        <input type="text" id="officePhone" v-model="office.officePhone">
+       
+       <label for="officeEmail">Email: </label>
+        <input type="email" id="officeEmail" v-model="office.officeEmail">
+       
+        <label for="officeOpenHours">Opening Time: </label>
+        <input type="time" id="officeOpenHours" v-model="office.officeOpenHours">
+       
+        <label for="officeCloseHours">Closing Time: </label>
+        <input type="time" id="officeCloseHours" v-model="office.officeCloseHour">
+       
+        
+        </form>
+       
+        <button></button>
     </div>
   </div>
 </template>
@@ -27,7 +63,20 @@ export default {
         firstName: "martin",
         Address: "1001 Spruce St",
         phoneNumber: "555-555-5555"
-}
+},
+office:{
+  officeName: "",
+  officeAddress: "",
+  officeCity: "",
+  officeState: "",
+  officeZipCode: "",
+  officePhoneNumber: "", 
+  officeEmail: "", 
+  officeStartHour: "", 
+  officeCloseHour: ""
+},
+addOffice: false,
+
     }
   },
 }
