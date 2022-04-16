252fd50d44ef328804f79d56037b68d7a891f967
Omari Bigham <Freemindhd@gmail.com>
2022-04-15 21:50:01 -0400

    Back on track

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,55 +1,47 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div id="app">
+    <div id="nav">
+      
+    <nav-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    </nav-bar>
+    </div>
+    
+    <router-view/>
+ 
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  </div>
 </template>
 
 <script>
-
+import NavBar from './components/NavBar.vue'
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  components: { 
+    NavBar },
+  name: 'app'
+}
 </script>
+
+
+
+
+<style>
+/* #app {
+  font-family: 'Lora', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  background-image: linear-gradient(90deg, white,  #E6E6FF, white);
+
+}
+
+.login-register-child{
+  display: inline-block;
+} */
+
+</style>
