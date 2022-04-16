9450f154274b08ffae1be7c01c7da2ae14d15822
Omari Bigham <Freemindhd@gmail.com>
2022-04-16 15:08:31 -0400

    avail serv obj added

---
+++ vue/src/services/AvailabilityService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: "http://localhost:3000"
+  });
+
+export default {
+
+    currentDoctorAvailability(){
+        return http.get('/availability')
+      },
+    currentDoctorAvailabilityByDay(dayOfWeek){
+        return http.get('/availability/${dayOfWeek}', dayOfWeek)
+      },
+}
