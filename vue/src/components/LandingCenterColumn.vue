bcf0f95690a1ea70cb3c352ab4f608fdd4ec7757
Omari Bigham <Freemindhd@gmail.com>
2022-04-19 06:48:29 -0400

    content style needed

--- vue/src/components/LandingCenterColumn.vue
+++ vue/src/components/LandingCenterColumn.vue
@@ -3,18 +3,58 @@
       <h1 class="welcome"></h1>
         <div class="lcc-sec-1">
         <div><img src="..\assets\family.png" alt="welcome"></div>
-        <p>Welcome to BookifyMd your one stop for medical appointment management</p> 
+        <div class="welcome-content"> <!--Welcome-->
+          <h1> Healthcare through human connections </h1> 
+          <h3> BookifyMDis a portal that integrates the day-to-day busy work and frees your staff and to keep the focus on what matters most: providing the best patient care. </h3>
+         <span>Welcome to BookifyMd your one stop for medical appointment management</span> 
+        </div>
         </div>
       <h1 class="about-us"></h1>
         <div class="lcc-sec-2">
           <img src="..\assets\hands.png" alt="read more about us">
-          <p>About us we are about you and you and us together make we!</p>
+          <div class="about-us-content"> <!--About Us-->
+              <h3>What can BookifyMD do for you?</h3>
+              <h3>improves patient engagement and provider responsiveness.</h3>
+              <h3>Convenient access to provider reviews and appointments.</h3>
+              <h3>Empower your patients to access their health and care information whenever they need it.</h3>
+
+              <h3>24/7 online scheduling</h3>
+              <h3>Make it easy for patients to digitally schedule and check-in for appointments at their convenience. Automated reminders support accountability and schedule density.</h3>
+              <h3>Responsive notifications to visualize agenda and optimize communication between doctors and patients.</h3>
+              <h3>Reduce no-shows with patient driven scheduling, outreach messaging, and prompts within the patient portal.</h3>
+              <h3>Reduce staff time spent on patient outreach with automated messaging at scale. Address care gaps and encourage specific health and payment-related actions.
+                  
+              </h3>
+</div>
         </div>
       <h1 class="testimonials"></h1>
         <div class="lcc-sec-3">
         <img src="..\assets\1on1.png" alt="testimonials">
-        <p>"I love BookifyMD, and use it for all my appointments"
-          -Rupert</p>
+        <div class="testimonial">
+
+          <h1>The benefits of engaging patients through BookifyMD</h1>
+          <p>
+          “the service we've received from BookifyMD leadership has been beyond belief.” -Hospitals of the University of Fennsylvania-Fenn Presbyterian
+          </p>
+          <p>
+          “BookifyMD has been easy for providers and patients to use, making healthcare more accessible.” -Fefferson Health-Thomas Fefferson University Hospital
+          </p>
+          “Implementing BookifyMD was a massive success for our organization, which was struggling with another telehealth platform during the peak of the pandemic shutdowns.” -Fennsylvania Hospital
+          <p>
+          “Selecting BookifyMD was, undoubtedly, a decision we only wish we initiated earlier. The workflow is effortless, and produces revenue as promised.” -Femple University Hospital
+          </p>
+          <p>
+          “ This systems from  BMS has produced an incalculable ROI in the short time of it’s application  and become a quintessential part of our practice in terms of maximized productivity and workflow." ” -Fahnemann University Hospital
+          </p>
+          </div>
+
+          <div>
+          <p>
+          Why Spend Vital Care Hours Scheduling Patients
+          BookifyMD make’s it convenient for patients to schedule or modify their appointments 24/7/365.
+          </p>
+          </div>
+
         </div>
     </div>
 </template>
