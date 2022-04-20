57681b1c699ad198d7ddec220b67d59861abb569
Eric <ehilkowitz@gmail.com>
2022-04-20 15:53:17 -0400

    update to availabilityService

--- vue/src/services/AvailabilityService.js
+++ vue/src/services/AvailabilityService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export default {
     currentDoctorAvailability(doctorId){
-        return axios.get('/doctors/${doctorId}/availability', doctorId)
+        return axios.get(`/doctors/${doctorId}/availability`)
       },
     currentDoctorAvailabilityByDay(dayOfWeek){
-        return axios.get('/availability/${dayOfWeek}', dayOfWeek)
-      },
+        return axios.get(`/availability/${dayOfWeek}`)
+      }
 }
