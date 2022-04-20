c69ebaf5e5b93bb62974507cfe615c47db948264
Eric <ehilkowitz@gmail.com>
2022-04-20 15:47:23 -0400

    update to notificationcolumn

--- vue/src/components/NotificationColumn.vue
+++ vue/src/components/NotificationColumn.vue
@@ -3,13 +3,19 @@
         <h1 class="agenda">Notifications</h1>
         <tbody>
             <tr v-for="notification in notifications" :key="notification.id">
+                <br><br>
                 <td> {{notification.alert}} </td>
-                <td> {{notification.date}} </td>
+                <br><br>
+                <br><br>
                 <td> {{notification.time}} </td>
+                <br><br>
                 <td> {{notification.doctor}} </td>
+                <br><br>
                 <td> {{notification.facility}} </td>
+                <br><br>
                 <td> {{notification.location}} </td>
-                <td> {{notification.description}} </td>
+                <br><br>
+                <!-- <td> {{notification.description}} </td> -->
             </tr>
         </tbody>
     </div>
@@ -29,9 +35,27 @@ data() {
         Location: "1001 Spruce St",
         Description: "Spinal Tap"
         }, 
-        {},
-        {},
-        {}
+        { alert: "On Time",
+        date: "05/01/22",
+        time: "1600",
+        doctor: "Naval Poindexter",
+        facility: "Presbyterian",
+        Location: "1001 Spruce St",
+        Description: "Spinal Tap"},
+        { alert: "On Time",
+        date: "05/01/22",
+        time: "1600",
+        doctor: "Naval Poindexter",
+        facility: "Presbyterian",
+        Location: "1001 Spruce St",
+        Description: "Spinal Tap"},
+        { alert: "Delay",
+        date: "04/20/22",
+        time: "30 Min Delay ",
+        doctor: "Dr Jolly",
+        facility: "Presbyterian",
+        Location: "1001 Spruce St",
+        Description: "Spinal Tap"}
       ] 
     }
   },
